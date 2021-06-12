c4facc96a22c8c976f721f8a0920b34edf85cf07
alamasyarie <maiaelarsya@gmail.com>
2021-06-12 14:40:24 +0700

    Layout  and search bar

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -3,7 +3,12 @@ import { coin_search, coin_input } from "./Search.module.css";
 const SearchBar = ({ ...rest }) => {
   return (
     <div className={coin_search}>
-      <input className={coin_input} type="text" placeholder="Search" />
+      <input
+        className={coin_input}
+        type="text"
+        placeholder="Search"
+        {...rest}
+      />
     </div>
   );
 };
